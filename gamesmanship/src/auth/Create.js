89404b010085482cb33530d254206a5b7fbd8762
Jamie Coakley <jamiemcoakley@gmail.com>
2020-08-25 18:37:57 -0400

    client first push

---
+++ gamesmanship/src/auth/Create.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const Create = (props) => {
+    const [gamername, setGamername] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetch('http://localhost:3000/gamer/create', {
+            method: "POST",
+            body: JSON.stringify({gamer: {gamername: gamername, password: password}}),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        }) .then(
+            (response) => response.json()
+        ) .then((data) => {
+            props.updateToken(data.sessionToken);
+        })
+    }
+
+    return(
+        <div>
+            <h1>CREATE A GAME LOG</h1>
+            <form onSubmit={handleSubmit} className="create-form">
+                <TextField
+                    className="create-gamername"
+                    label="GAMERNAME"
+                    required
+                    variant="outlined"
+                    id="gamernameInput"
+                    onChange={(e) => setGamername(e.target.value)}
+                />
+
+                <TextField
+                    className="create-password"
+                    label="PASSWORD"
+                    required
+                    variant="outlined"
+                    id="validation-outlined-input"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <Button type="submit">CREATE</Button>
+            </form>
+        </div>
+    )
+}
+
+export default Create;
---
+++ gamesmanship/src/auth/Login.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+// import {
+//     fade,
+//     ThemeProvider,
+//     withStyles,
+//     makeStyles,
+//     createMuiTheme,
+//   } from '@material-ui/core/styles';
+// import InputBase from '@material-ui/core/InputBase';
+// import InputLabel from '@material-ui/core/InputLabel';
+// import FormControl from '@material-ui/core/FormControl';
+//import { green, purple } from '@material-ui/core/colors';
+
+const Login = (props) => {
+    const [gamername, setGamername] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetch('http://localhost:3000/gamer/login', {
+            method: "POST",
+            body: JSON.stringify({gamer: {gamername: gamername, password: password}}),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        }) .then(
+            (response) => response.json()
+        ) .then((data) => {
+            props.updateToken(data.sessionToken);
+        })
+    }
+
+    return(
+        <div>
+            <h1>ACCESS YOUR GAME LOG</h1>
+            <form onSubmit={handleSubmit} className="login-form">
+                <TextField
+                    className="login-gamername"
+                    label="GAMERNAME"
+                    required
+                    variant="outlined"
+                    id="gamernameInput"
+                    onChange={(e) => setGamername(e.target.value)}
+                />
+
+                <TextField
+                    className="login-password"
+                    label="PASSWORD"
+                    required
+                    variant="outlined"
+                    id="validation-outlined-input"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <Button type="submit">LOG IN</Button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
